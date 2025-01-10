42dcb797b553ed49d9e03733798df181cc155f16
ibricchi <19757722+ibricchi@users.noreply.github.com>
2025-01-10 23:44:37 +0000

    add fly scene

--- scripts/swamp.gd
+++ scripts/swamp.gd
@@ -1,9 +1,16 @@
 extends Node
 
+@export var number_to_spwan: int = 10
+@export var random_x_start: int = 0
+@export var random_x_end: int = 500
+@export var random_y_start: int = 0
+@export var random_y_end: int = 300
+
+@onready var fly_res: Resource = preload("res://scenes/fly.tscn")
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#for i in 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
