8e68d8888ab1aa607e8f8cc670aa66454205da84
Vincent Sietses <vincent.sietses@gmail.com>
2025-01-11 16:23:37 +0100

    fly animation

--- scripts/fly.gd
+++ scripts/fly.gd
@@ -5,11 +5,15 @@ var wander_time : float = 3.0
 var rotation_speed : float = 2
 var cruise_velocity : float = 40
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	self.modulate = Color( 1 - 0.4 *randf(),1 - 0.4 *randf(),1 - 0.4 *randf() )
 	wander_orientation =  randf() * (2 * PI)
 	linear_damp = 0.5
-
+	_animated_sprite.play("fly")
+ 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -19,6 +23,7 @@ func _physics_process(delta: float) -> void:
 		wander_time = 5.0 * randf() + 1.5
 		wander_orientation =  randf() * (2 * PI)
 		cruise_velocity = 20 + randf()*40
+		_animated_sprite.speed_scale = cruise_velocity / 40
 	 
 	self.move_and_collide(
 		delta*cruise_velocity * tanh(wander_time)
