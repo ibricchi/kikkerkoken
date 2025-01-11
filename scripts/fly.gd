0936f37a82b62965fd59f727e9f2ded83a147cb8
Vincent Sietses <vincent.sietses@gmail.com>
2025-01-11 01:23:33 +0100

    flies twirl now

--- scripts/fly.gd
+++ scripts/fly.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 
 var wander_orientation : float = 0
 var wander_time : float = 4.0 
+var rotation_speed : float = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +11,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	self.global_rotation  = self.wander_orientation  
+func _process(delta: float) -> void:   
+	self.global_rotation = lerp_angle( self.global_rotation , self.wander_orientation  , rotation_speed * delta)
 	wander_time -= delta
 	if wander_time < 0:
 		wander_time = 4.0
