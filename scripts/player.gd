152dd7877540fe87bcab26fd855f5ce8c6531858
ibricchi <19757722+ibricchi@users.noreply.github.com>
2025-01-11 00:15:57 +0000

    Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,30 @@
 extends CharacterBody2D
 
-@export var speed: int = 200
-@export var dash_speed: int = 2000
+@export var max_speed: int = 200
+@export var acceleartion: float = 0.01
+@export var deceleration: float = 0.03
+
+var speed: Vector2 = Vector2.UP
 
 func get_movement_input():
-	var velocity = Vector2()
+	var input = Vector2()
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
+		input.x += 1
 	if Input.is_action_pressed("left"):
-		velocity.x -= 1
+		input.x -= 1
 	if Input.is_action_pressed("down"):
-		velocity.y += 1
+		input.y += 1
 	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-	return velocity.normalized()
+		input.y -= 1
+	return input.normalized()
 
 func _physics_process(delta):
-	velocity = get_movement_input() * speed
+	var input = get_movement_input()
+	if (input == Vector2.ZERO):
+		velocity -= velocity * deceleration
+	else:
+		var speed = velocity.length()
+		var new_speed = clampf(speed + max_speed * acceleartion, 0, max_speed)
+		velocity = input * new_speed
+		rotation = atan2(input.y, input.x)
 	move_and_slide()
